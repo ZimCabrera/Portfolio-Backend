5eff023c0c87a790919e0b777350c9e7622f3382
Cynthia Cabrera <cynthiacabrera1088@gmail.com>
2023-02-08 08:16:47 -0300

    modificaciones

--- src/main/java/com/portfolio/CynthiaMC/Entity/Persona.java
+++ src/main/java/com/portfolio/CynthiaMC/Entity/Persona.java
@@ -12,16 +12,14 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 
-@Getter @Setter
+
 @Entity
 public class Persona implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
     @Size(min = 1, max = 100, message = "no cumple con la longitud")
@@ -31,11 +29,11 @@ public class Persona implements Serializable{
     @Size(min = 1, max = 100, message = "no cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 100, message = "no cumple con la longitud")
+    @NotNull
+    private String descripcion;
+    
     private String img;
     
-    @Size(min = 1, max = 700, message = "no cumple con la longitud")
-    private String descripcion;
     
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEdu")
     private List<Educacion> educacionList;
@@ -47,13 +45,55 @@ public class Persona implements Serializable{
     public Persona() {
     }
 
-    public Persona(Long id, String nombre, String apellido, String img, String descripcion) {
-        this.id = id;
+    public Persona(String nombre, String apellido, String descripcion, String img) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.descripcion = descripcion;
         this.img = img;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    
+    
     
     
 }
